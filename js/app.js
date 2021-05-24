c1040a0a245d47862239b95ca7f1c60d2105e916
DoaaDaban <doaadaban993@gmail.com>
2021-05-24 22:00:34 +0300

    push AboutMe

---
+++ js/app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+let userName= prompt("Hello, whats ur name?");
+console.log(userName);
+
+alert("Hello " + userName + " welcome to My webPage");
+
+
+    let favColor= prompt('Do you think my Fav color is white? (Y/N)');
+    
+//    // console.log(favColor=favColor.toUpperCase());
+
+//         if (favColor.toUpperCase === ( "yes" | "y") ) {
+
+//             //console.log (favColor)
+//             alert ("Hmm! yes! My fav color is white")
+//         }
+
+        
+
+//         // else if (favColor.toUpperCase === "NO" | "N" ){
+//         //     console.log (favColor);
+//         //     alert ("Hmm! no! My fav color is white");
+//         // }
+
+//         else {
+//            // let answer=prompt("please answer y or n or yes or no only" )
+//             alert("Try again!")
+//         }
+
+        
+
+ let favDrink= prompt("Do you think my fav drink is coffee? (Y/N)");
+    //console.log(favDrink=favDrink.toUpperCase());
+
+    // switch(favDrink.toUpperCase == yes | y){
+
+    //     case yes | y:
+    //         alert ("Hmm! Yes! My fav drink is cofee")
+    //         break;
+
+    //      case no | n:
+    //         alert ("Hmm! NO! My fav drink is cofee")
+    //         break;
+
+
+    // }
+
+      
+
+
+
+ let favMeal= prompt("Do you think my fav Meal is pizza? (Y/N)");
+ //console.log(favMeal=favMeal.toUpperCase());
+
+ alert ("Hmm! My fav meal is pasta");
+
+ let favDessert= prompt("Do you think my fav dessert is chocolate? (Y/N)");
+ //console.log(favDessert=favDessert.toUpperCase());
+
+ alert ("Hmm! My fav dessert is chocolate");
+
+let favMusic= prompt("Do you think my fav music is soft music? (Y/N)");
+ //console.log(favMusic=favMusic.toUpperCase());
+ 
+ alert ("Hmm! My fav music is pop music");
+
+
+alert(" Thanks " + userName + " For visiting my webPage! and u welcome anyTime")
