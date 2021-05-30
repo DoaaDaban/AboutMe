d60ea6cfe0cb74be8c6b3a4ec5bc0ad387d4ed52
DoaaDaban <doaadaban993@gmail.com>
2021-05-30 12:54:03 +0300

    delete consolog

--- js/app.js
+++ js/app.js
@@ -2,24 +2,21 @@
 
 
 let userName= prompt("Hello, whats ur name?");
-console.log(userName);
+
 
 alert("Hello " + userName + " welcome to My webPage");
 
        let score=0;
-
-       //Q1
        function question1(){
     let ans= prompt('Do you think my Fav color is white? (Y/N)');
     let favColor =  ans.toLowerCase();
 
         if(favColor === "yes" || favColor === "y")  {
-           //console.log (favColor);
             alert ("Hmm! yes! My fav color is white")
             score++;
         }
         else if(favColor === "no" || favColor === "n" ){
-            // console.log (favColor);
+           
             alert ("Hmm! no! My fav color is white");
         }
         else {
@@ -30,20 +27,17 @@ alert("Hello " + userName + " welcome to My webPage");
     }
     question1();
 
-     //Q2
+    
       function question2(){
       let ans2= prompt("Do you think my fav drink is coffee? (Y/N)");
       let favDrink =  ans2.toLowerCase();
 
       if(favDrink === "yes" || favDrink === "y")  {
-
-          //console.log (favDrink)
           alert ("Hmm! yes! My fav drink is coffee")
           score++;
       }
   
       else if(favDrink === "no" || favDrink === "n" ){
-          // console.log (favDrink);
           alert ("Hmm! no! My fav drink is coffee");
 
       }
@@ -55,7 +49,7 @@ alert("Hello " + userName + " welcome to My webPage");
     }
      question2();
 
-   //Q3
+  
    function question3(){
 
     let ans3= prompt("Do you think my fav Meal is pasta? (Y/N)");
@@ -63,13 +57,11 @@ alert("Hello " + userName + " welcome to My webPage");
 
     if(favMeal === "yes" || favMeal === "y")  {
 
-     //console.log (favMeal)
      alert ("Hmm! yes! My fav Meal is pasta")
      score++;
  }
 
  else if(favMeal === "no" || favMeal === "n" ){
-     // console.log (favMeal);
      alert ("Hmm! no! My fav Meal is pasta");
  }
 
@@ -80,21 +72,18 @@ alert("Hello " + userName + " welcome to My webPage");
    }
    question3();
 
- //Q4
+
  function question4(){
  let ans4= prompt("Do you think my fav dessert is chocolate? (Y/N)");
  let favDessert =  ans4.toLowerCase();
 
  if(favDessert === "yes" || favDessert === "y")  {
-
-     //console.log (favDessert)
      alert ("Hmm! yes! My fav dessert is chocolate")
      score++;
  }
 
 
  else if(favDessert === "no" || favDessert === "n" ){
-     // console.log (favDessert);
      alert ("Hmm! no! My fav dessert is chocolate");
  }
 
@@ -106,20 +95,17 @@ alert("Hello " + userName + " welcome to My webPage");
  }
  question4();
 
-// Q5
+
 function question5(){
 
 let ans5= prompt("Do you think my fav music is pop music? (Y/N)");
 let favMusic =  ans5.toLowerCase();
 
 if(favMusic === "yes" || favMusic === "y")  {
-
-    //console.log (favMusic)
     alert ("Hmm! yes! My fav music is pop")
     score++;
 }
 else if(favMusic === "no" || favMusic === "n" ){
-    // console.log (favMusic);
     alert ("Hmm! no! My fav music is pop");
 }
 
@@ -133,17 +119,16 @@ alert(" Thanks " + userName + " For visiting my webPage! and u welcome anyTime")
 }
 question5();
 
-//  question 6 lab3
+
 function question6(){
 for (let i=0; i<4; i++){
     let numUser= Number(prompt("please enter any number between 1 to 6"));
     if (numUser === 1){
       alert("ur guess is too low");
-      // numUser= Number(prompt("try ur next attempt!")); 
     }
     else if (numUser === 2 || numUser === 4){
         alert("ur guess is too close");
-        // numUser= Number(prompt("try ur next attempt!"));    
+     
     }
     else if (numUser === 5 || numUser === 6){
         alert("ur guess is too high");
